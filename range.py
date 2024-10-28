bac411bfc961b46a54bca4eeb2076fa8068befb2
Haymanot Asmare <hasmare463@gmail.com>
2024-10-28 04:25:15 -0700

    Delete range.py

--- range.py
+++
@@ -1,8 +0,0 @@
-# for i in range(3,10):
-#     print(i)
-
-def great(name,time):
-    print(f"Good {time} {name},hop you are well")
-n=input("Enter your name: ")
-t = input("Enter time: ")
-great(n,t)
